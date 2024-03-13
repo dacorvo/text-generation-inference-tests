a4ddebf41a9d2e698f7b16599062c3b2faa08516
David Corvoysier <david@huggingface.co>
2024-03-13 12:35:57 +0000

    fix: typos

--- sagemaker_llm_container/utils/get_metrics.py
+++ sagemaker_llm_container/utils/get_metrics.py
@@ -48,10 +48,10 @@ def extract_metrics(input_string):
     return parsed_kpis
 
 
-def calcluate_throughput(avg_latency, user_count, duration=90, num_gen_tokens=50):
+def calculate_throughput(avg_latency, user_count, duration=90, num_gen_tokens=50):
     request_per_user_in_duration = ((duration * 1000) / avg_latency) * user_count
-    thorughput = (request_per_user_in_duration / duration) * num_gen_tokens
-    return thorughput
+    throughput = (request_per_user_in_duration / duration) * num_gen_tokens
+    return throughput
 
 
 def get_metrics_from_cloudwatch(
@@ -97,7 +97,7 @@ def get_metrics_from_cloudwatch(
 
     df = pd.DataFrame.from_records(metrics)
 
-    throughput_gen_per_s = calcluate_throughput(df["total_time_ms"].mean(), int(vu))
+    throughput_gen_per_s = calculate_throughput(df["total_time_ms"].mean(), int(vu))
 
     # get quantization
     if quantize:
@@ -116,7 +116,7 @@ def get_metrics_from_cloudwatch(
         "Do Sample": True,
         "Number of requests": len(df),
         "Virtual Users": int(vu),
-        "Thorughput (tokens/second)": throughput_gen_per_s,
+        "Throughput (tokens/second)": throughput_gen_per_s,
         "Latency (ms/token) avg": df["time_per_token_ms"].mean(),
         "Latency (ms/token) min": df["time_per_token_ms"].min(),
         "Latency (ms/token) med": df["time_per_token_ms"].median(),
@@ -129,12 +129,12 @@ def get_metrics_from_cloudwatch(
         "Latency Request ms min": df["total_time_ms"].min(),
         "Latency Request ms med": df["total_time_ms"].median(),
         "Latency Request ms max": df["total_time_ms"].max(),
-        "Latency Infernece ms med": df["inference_time_ms"].median(),
-        "Latency Infernece ms max": df["inference_time_ms"].max(),
-        "Latency Infernece ms p(90)": df["inference_time_ms"].quantile(0.9),
-        "Latency Infernece ms p(95)": df["inference_time_ms"].quantile(0.95),
-        "Latency Infernece ms avg": df["inference_time_ms"].mean(),
-        "Latency Infernece ms min": df["inference_time_ms"].min(),
+        "Latency Inference ms med": df["inference_time_ms"].median(),
+        "Latency Inference ms max": df["inference_time_ms"].max(),
+        "Latency Inference ms p(90)": df["inference_time_ms"].quantile(0.9),
+        "Latency Inference ms p(95)": df["inference_time_ms"].quantile(0.95),
+        "Latency Inference ms avg": df["inference_time_ms"].mean(),
+        "Latency Inference ms min": df["inference_time_ms"].min(),
         "Queue time ms med": df["queue_time_ms"].median(),
         "Queue time ms max": df["queue_time_ms"].max(),
         "Queue time ms p(90)": df["queue_time_ms"].quantile(0.9),
